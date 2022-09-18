626450eea585e4a6dd75329572209eb6ff829c67
Shahrik Amin <shahrikamin2001@gmail.com>
2022-09-18 19:20:02 -0400

    Added upon main() method code. Created a buddyInfo class and and added and removed it from the collection.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,18 +1,22 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class AddressBook {
 
-    HashMap<Integer, BuddyInfo> objectCollection = new HashMap<>();
+    ArrayList<BuddyInfo> objectCollections = new ArrayList<BuddyInfo>();
 
     // check these two methods
-    public void addBuddy(int collectionPlace,BuddyInfo obj){
-        objectCollection.put(collectionPlace, obj);
+    public void addBuddy(BuddyInfo obj){
+        objectCollections.add(obj);
     }
-    public void removeBuddy(int collectionPlace, BuddyInfo obj){
-        objectCollection.remove(collectionPlace, obj);
+    public void removeBuddy(BuddyInfo obj){
+        objectCollections.remove(obj);
     }
 
     public static void main(String[] args) {
         System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
